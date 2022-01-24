f92449a4de45c115fa97f0c43acd8760d4903e20
muzaffarmhm <muzaffarmhm@gmail.com>
2022-01-24 21:22:15 +0530

    Small Changes

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 const thread = require('./models/thread.js');
+const methodOverride = require('method-override');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname,'public')));
 app.use(express.urlencoded({extended:false}));
+app.use(methodOverride('_method'))
 
 mongoose.connect('mongodb://localhost/experto',{useNewUrlParser:true,useUnifiedTopology:true})
 .then(()=>{
@@ -31,6 +33,30 @@ app.get('/thread/:id',async(req,res)=>{
 
 app.get('/new',(req,res)=>{
     res.render('new');
+})
+
+app.post('/new',async(req,res)=>{
+    await thread.create(req.body);
+    res.redirect('/');
+})
+
+app.get('/thread/:id/edit',async(req,res)=>{
+    const id = req.params.id;
+    const threads = await thread.findById(id);
+    res.render('edit', {threads});
+})
+
+app.put('/:id/edit',async(req,res)=>{
+    const id = req.params.id;
+    await thread.findByIdAndUpdate(id,req.body);
+    res.redirect('/');
+
+})
+
+app.delete('/thread/:id/delete',async(req,res)=>{
+    const id = req.params.id;
+    await thread.findByIdAndDelete(id);
+    res.redirect('/');
 })
 
 app.listen(3000,()=>{
